c8c81047362e10c3f95f3b14fcc982177e966824
thesst24 <the.sst12@gmail.com>
2025-11-07 11:48:54 +0700

    add github profile edit header

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,19 +3,25 @@ export default function Contact() {
     <section id="contact" className="py-20 bg-gray-900 text-white text-center">
       <h2 className="text-3xl font-bold mb-6">Get In Touch</h2>
       <p className="mb-6">Let’s build something great together!</p>
-      <a
+      <div className="flex flex-row gap-3 justify-center items-center">
+        <a
         href="mailto:the.sst12@gmail.com"
-        className="bg-blue-500 px-6 py-3 rounded text-white hover:bg-blue-600 mr-5"
-      >
+        className="bg-blue-500 px-6 py-3 rounded text-white hover:bg-blue-600">
         Contact Me
       </a>
       <a
         href="https://wa.me/02058419955"
         target="_blank"
-        className="bg-blue-500 px-6 py-3 rounded text-white hover:bg-blue-600"
-      >
+        className="bg-blue-500 px-6 py-3 rounded text-white hover:bg-blue-600">
         020 58419955
       </a>
+      <a
+        href="https://github.com/thesst24"
+        target="_blank"
+        className="bg-blue-500 px-6 py-3 rounded text-white hover:bg-blue-600">
+        GitHub Profile
+      </a>
+      </div>
     </section>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-6 bg-gray-900 text-white">
+    <header className="flex justify-between items-center p-6 bg-gray-900 text-white sticky top-0 z-100">
       <h1 className="text-xl font-bold">MyPortfolio</h1>
       <nav className="space-x-6">
+        <Link href="/">Home</Link>
         <Link href="#about">About</Link>
+        <Link href="#projects">Project</Link>
         <Link href="#contact">Contact</Link>
       </nav>
     </header>
